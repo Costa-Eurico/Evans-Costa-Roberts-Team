c1b91406ce70109fc426f8b2bb86292f8ebd58c8
Eurico Costa <ejcosta.byui@gmail.com>
2015-06-24 12:56:56 -0600

    Converted to enumeration.

--- Charcoaled/charcoaled/src/byui/cit260/charcoaled/model/Actor.java
+++ Charcoaled/charcoaled/src/byui/cit260/charcoaled/model/Actor.java
@@ -5,6 +5,7 @@
  */
 package byui.cit260.charcoaled.model;
 
+import java.awt.Point;
 import java.io.Serializable;
 import java.util.Objects;
 
@@ -12,72 +13,25 @@ import java.util.Objects;
  *
  * @author eucosta
  */
-public class Actor implements Serializable{
+public enum Actor implements Serializable{
+    
+    FirefighterOne("Male Firefighter"),
+    FirefighterTwo("Female Firefighter");
     
     //class instance variables
-    private String name;
-    private String description;
-    private Location coordinates = new Location();
-
-    public Actor() {
-    }
+    private final String description;
+    private final Point coordinates;
     
-    public String getName(){
-        return name;
-    }
-    
-    public void setName(String name){
-        this.name = name;
+    Actor(String description){
+        this.description = description;
+        coordinates = new Point(1,1);
     }
     
     public String getDescription(){
         return description;
     }
     
-    public void setDescription(String name){
-        this.description = description;
-    }
-    
-    public Location getCoordinates() {
+    public Point getCoordinates(){
         return coordinates;
-    }
-
-    public void setCoordinates(Location coordinates) {
-        this.coordinates = coordinates;
-    }
-
-    @Override
-    public String toString() {
-        return "Actor{" + "name=" + name + ", description=" + description + ", coordinates=" + coordinates + '}';
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 23 * hash + Objects.hashCode(this.name);
-        hash = 23 * hash + Objects.hashCode(this.description);
-        hash = 23 * hash + Objects.hashCode(this.coordinates);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final Actor other = (Actor) obj;
-        if (!Objects.equals(this.name, other.name)) {
-            return false;
-        }
-        if (!Objects.equals(this.description, other.description)) {
-            return false;
-        }
-        if (!Objects.equals(this.coordinates, other.coordinates)) {
-            return false;
-        }
-        return true;
     }
 }
